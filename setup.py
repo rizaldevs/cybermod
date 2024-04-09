d9352a426486538af992a379fe5a2f8ed049413f
rizaldevs <rizaldaitona@gmail.com>
2024-04-09 14:49:57 +0000

    helo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cybermod",
+    version="5.4.1",
+    description="A monkeypatcher add-on for cybergram that does conversation handling",
+    author="rizaldevs",
+    author_email="rizaldaitona@gmail.com",
+    packages=find_packages(),
+    install_requires=["cybergram>=1.0"],
+    python_requires=">=3.8",
+)
+
