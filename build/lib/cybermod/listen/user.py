2e4cd4fa7d458ff786924fe61f2b220dc5f8bf3a
rizaldevs <rizaldaitona@gmail.com>
2024-04-09 14:51:55 +0000

    helo

---
+++ build/lib/cybermod/listen/user.py
@@ -0,0 +1,21 @@
+import cybergram
+
+from .client import Client
+from ..utils import patch_into, should_patch
+
+
+@patch_into(cybergram.types.user_and_chats.user.User)
+class User(cybergram.types.user_and_chats.user.User):
+    _client: Client
+
+    @should_patch()
+    def listen(self, *args, **kwargs):
+        return self._client.listen(*args, user_id=self.id, **kwargs)
+
+    @should_patch()
+    def ask(self, text, *args, **kwargs):
+        return self._client.ask(self.id, text, *args, user_id=self.id, **kwargs)
+
+    @should_patch()
+    def stop_listening(self, *args, **kwargs):
+        return self._client.stop_listening(*args, user_id=self.id, **kwargs)
